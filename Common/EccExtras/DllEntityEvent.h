d254eff2bbb5f182c08658fa8c91a1fb104a5ee4
Dreamy Cecil <stalker27production@gmail.com>
2022-12-22 07:38:02 +0300

    Reimplement CDLLEntityEvent class from 1.50 as an extra ECC feature.

--- Common/EccExtras/DllEntityEvent.h
+++ Common/EccExtras/DllEntityEvent.h
@@ -13,14 +13,27 @@ You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 
-#ifndef CECIL_INCL_ECCEXTRAS_H
-#define CECIL_INCL_ECCEXTRAS_H
+#ifndef CECIL_INCL_DLLENTITYEVENT_H
+#define CECIL_INCL_DLLENTITYEVENT_H
 
 #ifdef PRAGMA_ONCE
   #pragma once
 #endif
 
-// Include extra features used by the custom entity class compiler
-#include "EccExtras/DllEntityEvent.h"
+#if SE1_VER < 150
+
+#include <Engine/Entities/EntityEvent.h>
+
+// Reimplementation of DLL entity event class from 1.50
+class CDLLEntityEvent {
+  public:
+    SLONG dee_slEvent; // Event code
+    CEntityEvent *(*dee_New)(void); // Pointer to the event class constructor
+
+    // [Cecil] NOTE: Event class size to compensate for the lack of CEntityEvent::GetSizeOf virtual method
+    SLONG dee_slSizeOf;
+};
+
+#endif
 
 #endif
