fc211d59a64e2ff0e89cb539a0eb542fa3db4201
Dreamy Cecil <stalker27production@gmail.com>
2022-12-23 08:01:11 +0300

    Define extra features for ECC in the main library header. - Update "Includes" submodule.
    Now ECC will generate entity table entries for each entity class that will be added into the exported entity table upon library initialization.

--- Sources/Entities/LibMain.cpp
+++ Sources/Entities/LibMain.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2002-2012 Croteam Ltd. 
+/* Copyright (c) 2022 Dreamy Cecil
 This program is free software; you can redistribute it and/or modify
 it under the terms of version 2 of the GNU General Public License as published by
 the Free Software Foundation
@@ -14,6 +14,9 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 
 #include "StdH.h"
+
+// Define extra features for ECC
+#include <EccExtras.cpp>
 
 // Library entry point
 BOOL APIENTRY DllMain(HANDLE hModule, DWORD ulReason, LPVOID lpReserved) {
