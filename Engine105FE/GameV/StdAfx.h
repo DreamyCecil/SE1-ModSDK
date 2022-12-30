0a2ccb563e692610330870d7f5c628038d592dc5
Dreamy Cecil <stalker27production@gmail.com>
2022-12-30 19:01:30 +0300

    Upload headers & libraries of vanilla SDK for TFE 1.05.

---
+++ Engine105FE/GameV/StdAfx.h
@@ -0,0 +1,17 @@
+#include <Engine/Engine.h>
+#include <GameV/Game.h>
+
+/* rcg10042001 protect against Visual C-isms. */
+#ifdef _MSC_VER
+#define DECL_DLL _declspec(dllimport)
+#endif
+
+#ifdef PLATFORM_UNIX
+#define DECL_DLL 
+#endif
+
+#include <Entities/Global.h>
+#include <Entities/Common/Common.h>
+#include <Entities/Common/GameInterface.h>
+#include <Entities/Player.h>
+#undef DECL_DLL
