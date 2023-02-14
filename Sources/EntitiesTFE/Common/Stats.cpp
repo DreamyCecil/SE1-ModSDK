c5ef8ab989560d2308b293fac7af4bd9f8017027
Dreamy Cecil <stalker27production@gmail.com>
2023-02-15 00:31:22 +0300

    Add EntitiesTFE project with entity code for The First Encounter. - Fix .gitignores to include entity sources.

--- Sources/EntitiesTFE/Common/Stats.cpp
+++ Sources/EntitiesTFE/Common/Stats.cpp
@@ -12,30 +12,3 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
-
-#include <Engine/Engine.h>
-#include <Game/Game.h>
-#include <Game/SEColors.h>
-
-/* rcg10042001 protect against Visual C-isms. */
-#ifdef _MSC_VER
-#define DECL_DLL _declspec(dllimport)
-#endif
-
-#ifdef PLATFORM_UNIX
-#define DECL_DLL 
-#endif
-
-#if SE1_GAME == SS_TSE
-  #include <EntitiesTSE/Global.h>
-  #include <EntitiesTSE/Common/Common.h>
-  #include <EntitiesTSE/Common/GameInterface.h>
-  #include <EntitiesTSE/Players/Player.h>
-#else
-  #include <EntitiesTFE/Global.h>
-  #include <EntitiesTFE/Common/Common.h>
-  #include <EntitiesTFE/Common/GameInterface.h>
-  #include <EntitiesTFE/Players/Player.h>
-#endif
-
-#undef DECL_DLL
--- Sources/EntitiesTFE/StdH.h
+++ Sources/EntitiesTFE/StdH.h
@@ -14,28 +14,20 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 
 #include <Engine/Engine.h>
-#include <Game/Game.h>
-#include <Game/SEColors.h>
+#include <Game/SessionProperties.h>
+#include <Game/PlayerSettings.h>
 
 /* rcg10042001 protect against Visual C-isms. */
 #ifdef _MSC_VER
-#define DECL_DLL _declspec(dllimport)
+#define DECL_DLL _declspec(dllexport)
 #endif
 
 #ifdef PLATFORM_UNIX
 #define DECL_DLL 
 #endif
 
-#if SE1_GAME == SS_TSE
-  #include <EntitiesTSE/Global.h>
-  #include <EntitiesTSE/Common/Common.h>
-  #include <EntitiesTSE/Common/GameInterface.h>
-  #include <EntitiesTSE/Players/Player.h>
-#else
-  #include <EntitiesTFE/Global.h>
-  #include <EntitiesTFE/Common/Common.h>
-  #include <EntitiesTFE/Common/GameInterface.h>
-  #include <EntitiesTFE/Players/Player.h>
-#endif
-
-#undef DECL_DLL
+#include "Global.h"
+#include "Common/Flags.h"
+#include "Common/Common.h"
+#include "Common/Particles.h"
+#include "Common/GameInterface.h"
